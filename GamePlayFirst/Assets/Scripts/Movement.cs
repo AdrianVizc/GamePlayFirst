8dee097d6e7f6b89de51270997daf8d2e7797038
ahhhThomas <146419438+ahhhThomas@users.noreply.github.com>
2025-07-15 00:31:08 -0700

    accel on input, else natural decel

--- GamePlayFirst/Assets/Scripts/Movement.cs
+++ GamePlayFirst/Assets/Scripts/Movement.cs
@@ -7,15 +7,21 @@ public class Movement : MonoBehaviour
     //Keyboard Controls only for now
 
     [SerializeField] private float maxSpeed;
+    [SerializeField] private float minSpeed;
     [SerializeField] private float startingSpeed;
-    private float currentSpeed;
-    private float lateralSpeed;
-    private float brakeForce;
+    [SerializeField] private float currentSpeed;
+    [SerializeField] private float lateralSpeed;
+    [SerializeField] private float accelSpeed;
+    [SerializeField] private float decelSpeed;
+    [SerializeField] private float brakeForce;
     private Rigidbody rb;
     private Camera mainCamera;
     private Vector3 moveDirection;
     private float horizontalInput;
     private float verticalInput;
+    private bool isAccelerating;
+    private bool isBraking;
+    private bool isTurning;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +29,7 @@ public class Movement : MonoBehaviour
         rb = this.GetComponent<Rigidbody>();
         mainCamera = Camera.main;
         currentSpeed = startingSpeed;
+        rb.velocity = mainCamera.transform.forward * currentSpeed;
     }
 
     // Update is called once per frame
@@ -33,14 +40,37 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        GetInput();
         Move();
     }
 
     private void GetInput()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        //horizontalInput = Input.GetAxis("Horizontal");
+        //verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+            isAccelerating = true;
+        }
+        else
+        {
+            isAccelerating = false;
+        }
 
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            isBraking = true;
+        }
+        else
+        {
+            isBraking = false;
+        }
+
+        //if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        //{
+        //    isTurning = true;
+        //}
     }
     private void Move()
     {
@@ -48,11 +78,32 @@ public class Movement : MonoBehaviour
         //moveDirection = new Vector3(moveDirection.x, 0, moveDirection.z);
 
         //rb.velocity
-        Vector3 forwardForce = mainCamera.transform.forward * currentSpeed;
 
-        if (rb.velocity.magnitude < maxSpeed)
+        //Vector3 forwardForce = mainCamera.transform.forward * currentSpeed;
+
+        //if (rb.velocity.magnitude < maxSpeed)
+        //{
+        //    rb.AddForce(forwardForce, ForceMode.Acceleration);
+        //}
+        if (isAccelerating)
         {
-            rb.AddForce(forwardForce, ForceMode.Acceleration);
+            if (currentSpeed < maxSpeed)
+            {
+                currentSpeed += accelSpeed * Time.fixedDeltaTime;
+            }
         }
+        else if (isBraking)
+        {
+
+        }
+        else
+        {
+            if (currentSpeed > minSpeed)
+            {
+                currentSpeed -= decelSpeed * Time.fixedDeltaTime;
+            }
+        }
+
+        rb.velocity = mainCamera.transform.forward * currentSpeed;
     }
 }
