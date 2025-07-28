ca1307bd3f20ae81f1e9843770d5f2dd1ffbe654
dominic-tran <dominic-tran@users.noreply.github.com>
2025-07-27 21:12:08 -0700

    removed random header?????

--- GamePlayFirst/Assets/Scripts/Rail and Walls/PlayerGrind.cs
+++ GamePlayFirst/Assets/Scripts/Rail and Walls/PlayerGrind.cs
@@ -6,7 +6,6 @@ using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Splines;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerGrind : MonoBehaviour
 {
