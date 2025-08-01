201c04a9a804539ecb838ddb994502adec981322
dominic-tran <dominic-tran@users.noreply.github.com>
2025-08-01 00:47:35 -0700

    polished animation for bump

--- GamePlayFirst/Assets/Scripts/PlayerBump.cs
+++ GamePlayFirst/Assets/Scripts/PlayerBump.cs
@@ -14,7 +14,6 @@ public class PlayerBump : MonoBehaviour
     private Animator animator;
     private bool isBumping;
     private bool zeroVel;
-    private bool isCollidingIgnored;
 
     // private float currentSpeed;
     // private const float MAX_SPEED = 10f;
@@ -25,7 +24,6 @@ public class PlayerBump : MonoBehaviour
         movement = GetComponent<Movement>();
         animator = GetComponentInChildren<Animator>();
         zeroVel = false;
-        isCollidingIgnored = false;
     }
 
     void FixedUpdate()
@@ -41,7 +39,7 @@ public class PlayerBump : MonoBehaviour
             movement.enabled = true;
             AudioManager.instance.UnPauseSkateSound();
         }
-        else if (!isCollidingIgnored && Physics.CapsuleCast(bottom, top, raycastCheckRadius, transform.forward, out RaycastHit hit, raycastCheckDistance, ~0, QueryTriggerInteraction.Ignore))
+        else if (Physics.CapsuleCast(bottom, top, raycastCheckRadius, transform.forward, out RaycastHit hit, raycastCheckDistance, ~0, QueryTriggerInteraction.Ignore))
         {
             if (hit.collider.CompareTag("rail") || 
                 (hit.collider.CompareTag("wall") && !movement.isGrounded) &&
@@ -59,9 +57,13 @@ public class PlayerBump : MonoBehaviour
             rb.velocity = bumpDir * bumpForce;
 
             isBumping = true;
-            //animator.SetTrigger("getHurt");
 
-            StartCoroutine(IgnoreCollideTemporarily(0.1f));
+            AnimatorStateInfo animatorState = animator.GetCurrentAnimatorStateInfo(0);
+            if (!animatorState.IsName("Hurt") ||
+                (animatorState.IsName("Hurt") && animatorState.normalizedTime > 0.75f))
+            {
+                animator.SetTrigger("getHurt");
+            }
         }
 
         if(rb.velocity.z == 0)
@@ -79,12 +81,5 @@ public class PlayerBump : MonoBehaviour
             Vector3 addedGravity = Physics.gravity * (movement.gravityMultiplier - 1f);
             rb.AddForce(addedGravity, ForceMode.Acceleration);
         }
-    }
-
-    private IEnumerator IgnoreCollideTemporarily(float duration)
-    {
-        isCollidingIgnored = true;
-        yield return new WaitForSeconds(duration); // ignore rail tag for x sec
-        isCollidingIgnored = false;
     }
 }
